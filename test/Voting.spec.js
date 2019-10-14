e9f36b96329ee998818d15243119d214b547701b
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-10-14 23:17:27 +0530

    Added tests

---
+++ test/Voting.spec.js
@@ -0,0 +1,344 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+// Contracts
+const VotingFactory = require("../build/contracts/VotingFactory.json");
+const Voting = require("../build/contracts/Voting.json");
+
+const web3 = new Web3(
+  ganache.provider({
+    gasLimit: 8000000
+  })
+);
+
+const GAS_VALUE = "5000000";
+let accounts, factory, voting;
+
+before(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(VotingFactory.abi)
+    .deploy({ data: VotingFactory.bytecode, arguments: [] })
+    .send({ from: accounts[0], gas: GAS_VALUE });
+
+  // Create a campaign using factory
+  await factory.methods
+    .createCampaign(10000, "Campaign 1")
+    .send({ from: accounts[0], gas: GAS_VALUE });
+
+  /**
+   * Entry stages
+   * 0 - Rejected
+   * 1 - Not decided
+   * 2 - Accepted
+   */
+
+  const campaign = await factory.methods.getCampaign(1).call();
+  const address = campaign["0"];
+
+  voting = new web3.eth.Contract(Voting.abi, address);
+});
+
+describe("Voting Factory", () => {
+  it("deploys contract", () => {
+    assert.ok(factory.options.address);
+  });
+
+  it("creates campaign", async () => {
+    await factory.methods
+      .createCampaign(5000, "Campaign 2")
+      .send({ from: accounts[0], gas: GAS_VALUE });
+
+    const campaign = await factory.methods.getCampaign(2).call();
+
+    assert.strictEqual(campaign["1"], "Campaign 2");
+  });
+
+  it("only allows EC to create campaign", async () => {
+    let noErrFlag = false;
+
+    try {
+      await factory.methods
+        .createCampaign(5000, "Campaign 2")
+        .send({ from: accounts[1], gas: GAS_VALUE });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+});
+
+describe("Voting", () => {
+  it("deploys contract", () => {
+    assert.ok(voting.options.address);
+  });
+
+  it("marks sender as EC", async () => {
+    const EC = await voting.methods.electionCommission().call();
+
+    assert.strictEqual(EC, accounts[0]);
+  });
+
+  it("accepts entry by candidate", async () => {
+    await voting.methods.submitEntry("Candidate 1", 2, "IPFSHash").send({
+      from: accounts[1],
+      gas: GAS_VALUE,
+      value: "20000"
+    });
+
+    const { name } = await voting.methods.candidates(1).call();
+    const noOfCandidate = await voting.methods.noOfCandidates().call();
+
+    assert.strictEqual("Candidate 1", name);
+    assert.strictEqual(Number(noOfCandidate), 1);
+  });
+
+  it("rejects entry by EC", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.submitEntry("Candidate 2", 5, "IPFSHash").send({
+        from: accounts[0],
+        gas: GAS_VALUE,
+        value: "50000"
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("doesn't allow candidate entry with same name", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.submitEntry("Candidate 1", 2, "IPFSHash").send({
+        from: accounts[1],
+        gas: GAS_VALUE,
+        value: "20000"
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("candidate entry requires registration fee", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.submitEntry("Candidate 3", 2, "IPFSHash").send({
+        from: accounts[1],
+        gas: GAS_VALUE,
+        value: "10000"
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("doesn't allow candidate with 0 members", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.submitEntry("Candidate 3", 0, "IPFSHash").send({
+        from: accounts[1],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("allows EC to approve entry", async () => {
+    await voting.methods.approveEntry(1).send({
+      from: accounts[0],
+      gas: GAS_VALUE
+    });
+
+    const { name, stage } = await voting.methods.candidates(1).call();
+
+    assert.strictEqual(name, "Candidate 1");
+    assert.strictEqual(Number(stage), 2);
+  });
+
+  it("allows EC to reject entry", async () => {
+    // Create another candidate
+    await voting.methods.submitEntry("Candidate 2", 1, "IPFSHash").send({
+      from: accounts[1],
+      gas: GAS_VALUE,
+      value: "10000"
+    });
+
+    await voting.methods.rejectEntry(2).send({
+      from: accounts[0],
+      gas: GAS_VALUE
+    });
+
+    const { name, stage } = await voting.methods.candidates(2).call();
+
+    assert.strictEqual(name, "Candidate 2");
+    assert.equal(stage, 0);
+  });
+
+  it("doesn't allow invalid candidate id for approval", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.approveEntry(101).send({
+        from: accounts[0],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("doesn't allow random user to approve entry", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.approveEntry(1).send({
+        from: accounts[1],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("allows user to vote", async () => {
+    await voting.methods.vote(1).send({
+      from: accounts[2],
+      gas: GAS_VALUE
+    });
+
+    const { name, noOfVotes } = await voting.methods.candidates(1).call();
+
+    assert.strictEqual(name, "Candidate 1");
+    assert.strictEqual(Number(noOfVotes), 1);
+  });
+
+  it("allows user to vote only ONCE", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.vote(1).send({
+        from: accounts[2],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("allows user to vote only for eligible candidates", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.vote(2).send({
+        from: accounts[3],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("doesn't allow user to vote for candidate with invalid ID", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.vote(20).send({
+        from: accounts[3],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("allows EC to close campaign", async () => {
+    await voting.methods.closeCampaign().send({
+      from: accounts[0],
+      gas: GAS_VALUE
+    });
+
+    const completed = await voting.methods.completed().call();
+
+    assert(completed);
+  });
+
+  it("rejects entry after campaign is closed", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.submitEntry("Candidate 3", 5, "IPFSHash").send({
+        from: accounts[1],
+        gas: GAS_VALUE,
+        value: "50000"
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+
+  it("doesn't allow EC to approve entry after campaign is closed", async () => {
+    let noErrFlag = false;
+
+    try {
+      await voting.methods.approveEntry(1).send({
+        from: accounts[0],
+        gas: GAS_VALUE
+      });
+
+      noErrFlag = true;
+    } catch (err) {
+      assert.ok(err);
+    }
+
+    assert.strictEqual(noErrFlag, false);
+  });
+});
---
+++ test/VotingSimulation.spec.js
@@ -0,0 +1,112 @@
+/**
+ * Simulate the whole voting process,
+ * and ensures it behaves correctly
+ */
+
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+// Contracts
+const VotingFactory = require("../build/contracts/VotingFactory.json");
+const Voting = require("../build/contracts/Voting.json");
+
+const web3 = new Web3(
+  ganache.provider({
+    gasLimit: 8000000
+  })
+);
+
+const GAS_VALUE = "5000000";
+let accounts, factory, voting, EC;
+
+before(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  EC = accounts[0];
+
+  factory = await new web3.eth.Contract(VotingFactory.abi)
+    .deploy({ data: VotingFactory.bytecode, arguments: [] })
+    .send({ from: EC, gas: GAS_VALUE });
+
+  // Create a campaign using factory
+  await factory.methods
+    .createCampaign(10000, "Campaign 1")
+    .send({ from: EC, gas: GAS_VALUE });
+
+  const campaign = await factory.methods.getCampaign(1).call();
+
+  voting = new web3.eth.Contract(Voting.abi, campaign["0"]);
+});
+
+describe("Voting simulation", () => {
+  it("creates 3 candidates", async () => {
+    for (let i = 1; i <= 3; i++) {
+      await voting.methods.submitEntry(`Candidate ${i}`, i, "hash").send({
+        from: accounts[i],
+        gas: GAS_VALUE,
+        value: Number(i * 10000).toString()
+      });
+    }
+
+    const candidates = await voting.methods.noOfCandidates().call();
+
+    assert.strictEqual(Number(candidates), 3);
+  });
+
+  it("EC approves candidates for election", async () => {
+    for (let i = 1; i <= 3; i++) {
+      await voting.methods.approveEntry(i).send({
+        from: EC,
+        gas: GAS_VALUE
+      });
+    }
+  });
+
+  it("start voting", async () => {
+    // 1 voter for candidate 1
+    await voting.methods.vote(1).send({
+      from: accounts[4],
+      gas: GAS_VALUE
+    });
+
+    // 3 voters for candidate 2
+    for (let i = 6; i <= 8; i++) {
+      await voting.methods.vote(2).send({
+        from: accounts[i],
+        gas: GAS_VALUE
+      });
+    }
+
+    // 1 voter for candidate 3
+    await voting.methods.vote(3).send({
+      from: accounts[5],
+      gas: GAS_VALUE
+    });
+
+    const candidate1 = await voting.methods.candidates(1).call();
+    const candidate2 = await voting.methods.candidates(2).call();
+    const candidate3 = await voting.methods.candidates(3).call();
+
+    assert.strictEqual(Number(candidate1.noOfVotes), 1);
+    assert.strictEqual(Number(candidate2.noOfVotes), 3);
+    assert.strictEqual(Number(candidate3.noOfVotes), 1);
+  });
+
+  it("get correct winner", async () => {
+    const winningId = await voting.methods.getWinner().call();
+
+    assert.strictEqual(Number(winningId), 2);
+  });
+
+  it("close campaign", async () => {
+    await voting.methods.closeCampaign().send({
+      from: EC,
+      gas: GAS_VALUE
+    });
+
+    const campaignStatus = await voting.methods.completed().call();
+
+    assert(campaignStatus);
+  });
+});
